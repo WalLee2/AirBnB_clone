a2d564ccda3c786c5aebf9cde2f75df9eeb39374
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-07 21:20:20 -0800

    Found uuid Library for generating unique user id's

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-import datetime
+import datetime, uuid
 class BaseModel():
-    def __init__(self, uuid="", name="anomyous"):
+    def __init__(self, uuid=, name="anomyous"):
         self.uuid = uuid
         self.name = "anomyous"
-        self.created_at = datetime.datetime.now()
+        self.created_at = "{}".format(datetime.datetime.now().isoformat())
+        self.updated_at = "{}".format(datetime.datetime.now().isoformat())
 
+	@setter.save
     def save(self):
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        return()
+        return("[{}] {} {}".format(type(self), self.uuid, BaseModel().__dict__))
 
 new = BaseModel()
 print(new)
