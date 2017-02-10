ad3b26f5bfe4c14cf5e427481707ee46dc16c07c
Walton Lee <mr.waltonlee@me.com>
2017-02-10 21:22:27 +0000

    serializing and deserializing datetime objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """
 
 """
+from datetime import datetime
 import json
 from os.path import isfile
 
@@ -13,16 +14,28 @@ class FileStorage():
     __file_path = "file.json"
 
     def all(self):
-        return __objects
+        return FileStorage.__objects
 
     def new(self, obj):
         self.__objects.update({obj.id: obj})
 
     def save(self):
-        with open(__file_path, 'w', encoding="UTF-8") as my_file:
-            json.dump(strftime(FileStorage.__objects), my_file)
+        new_cp = {}
+        for key in FileStorage.__objects:
+            if type(FileStorage.__objects[key]) is not dict:
+                new_cp.update({key: FileStorage.__objects[key].to_json()})
+            else:
+                new_cp.update({key: FileStorage.__objects[key]})
+                new_cp.update({'created_at': str(new_cp[key]['created_at'])})
+                new_cp.update({'updated_at': str(new_cp[key]['updated_at'])})
+        with open(FileStorage.__file_path, 'w', encoding="UTF-8") as my_file:
+            json.dump(new_cp, my_file)
 
     def reload(self):
         if isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r') as myFile:
-                return json.load(myFile)
+            with open(FileStorage.__file_path, 'r', encoding="UTF-8") as File:
+                FileStorage.__objects = json.load(File)
+                from models.base_model import BaseModel
+                for key in FileStorage.__objects.keys():
+                    new_BM = BaseModel(FileStorage.__objects[key])
+                    FileStorage.__objects.update({key: new_BM})
