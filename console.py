948b2c1a26cccfae9d16da611a4c28443b246d2c
Walton Lee <mr.waltonlee@me.com>
2017-02-11 20:44:10 +0000

    adding create, show and destroy functionality to the command line interpreter

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 Entry point to the command interpreter.
 """
 import cmd
+from models.__init__ import storage
+from models.base_model import BaseModel
 
 
 class Console(cmd.Cmd):
@@ -23,6 +25,69 @@ class Console(cmd.Cmd):
     def emptyline(self):
         return False
 
+    def do_create(self, user_input):
+        """Creates a new instance and returns the unique id"""
+        input_len = user_input.split()
+        if (input_len):
+            new_model = BaseModel()
+            new_model.save()
+            print(new_model.id)
+            storage.reload()
+        else:
+            print("Please specify an instance to be created.")
+
+    def do_show(self, usr_in):
+        """Prints the whole dictionary with the new instance"""
+        _input = usr_in.split()
+        switch = 0
+        switch1 = 0
+        objects = storage.all()
+        if _input[0] not in class_check:
+            print("** class doesn't exist **")
+        elif len(_input) < 2:
+            num_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+            for letter in _input[0]:
+                if letter in num_list:
+                    switch1 = 1
+            if switch1 == 1:
+                print("** class name missing **")
+            else:
+                print("** instance id missing **")
+        elif len(_input) == 2:
+            for key in objects.keys():
+                if key == _input[1]:
+                    print(objects[key])
+                    switch = 1
+            if switch == 0:
+                print("** no instance found **")
+
+    def do_destroy(self, usr_in):
+        """Destroys the instance with a specific id"""
+        _input = usr_in.split()
+        switch = 0
+        switch1 = 0
+        objects = storage.all()
+        if _input[0] not in class_check:
+            print("** class doesn't exist **")
+        elif len(_input) < 2:
+            num_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+            for letter in _input[0]:
+                if letter in num_list:
+                    switch1 = 1
+            if switch1 == 1:
+                print("** class name missing **")
+            else:
+                print("** instance id missing **")
+        elif len(_input) == 2:
+            for key in objects.keys():
+                if key == _input[1]:
+                    del objects[key]
+                    switch = 1
+                    break
+            storage.save()
+            if switch == 0:
+                print("** no instance found **")
+
     #################
     # Help Functions#
     #################
@@ -34,6 +99,8 @@ class Console(cmd.Cmd):
         print("CTL + D (EOF) to exit the program")
 
 if __name__ == "__main__":
+    class_check = ["BaseModel", "User", "State",
+                   "City", "Amenity", "Place", "Review"]
     prompt = Console()
     prompt.prompt = "(hbnb) "
     prompt.emptyline
