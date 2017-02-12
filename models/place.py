a53c087328ad91ec469fb83ead0144979f92568a
Walton Lee <mr.waltonlee@me.com>
2017-02-12 05:03:45 +0000

    adding comments, pep8 approved, and making Place class functional

--- models/place.py
+++ models/place.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+Creating a class: Place that inherits from BaseModel super class.
+"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """
+    Creating public class attributes that can be used in methods.
+    """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenities = []
+
+    def __init__(self, *args, **kwargs):
+        """Initiating the class"""
+        if (args and type(args) is dict):
+            BaseModel.__init__(self, args[0])
+        else:
+            BaseModel.__init__(self)
