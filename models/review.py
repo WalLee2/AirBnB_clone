63ccea1ac1bcb918c11ab6cbce812919585faa99
Walton Lee <mr.waltonlee@me.com>
2017-02-12 04:14:03 +0000

    adding a review class

--- models/review.py
+++ models/review.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+Review class inheriting from a super class BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    Creating public class attributes and defining an __init__ method.
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initiating the Review class"""
+        if (args and type(args) is dict):
+            BaseModel.__init__(self, args[0])
+        else:
+            BaseModel.__init__(self)
