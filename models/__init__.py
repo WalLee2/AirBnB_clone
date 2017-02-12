c509d2a49ff1d432e0169b337ab7b567dbc47b6f
Walton Lee <mr.waltonlee@me.com>
2017-02-12 05:03:16 +0000

    adding comments and pep8 approved

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-
+Importing FileStorage and setting that equal to storage
+and calling reload
 """
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
