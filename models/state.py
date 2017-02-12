3f35196d736824799e1ff8ec1a103234afbbd8cf
Walton Lee <mr.waltonlee@me.com>
2017-02-12 05:05:35 +0000

    adding comments, pep8 approved, and making state class functional

--- models/state.py
+++ models/state.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+Creating the class State and inheriting from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Creating a public instance which can be accessed in subsequent methods
+    """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initiating the class State to be able to accept arguments
+        """
+        if (args and type(args) is dict):
+            BaseModel.__init__(self, args[0])
+        else:
+            BaseModel.__init__(self)
