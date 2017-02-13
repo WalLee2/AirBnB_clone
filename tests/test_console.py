7d7970aa98262a2c6c484334616d2a8bf81c18a4
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-12 23:40:44 -0800

    Adding empty file for testing test

---
+++ tests/test_console.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Unit tests for the console
+Run with: python -m unittest tests/test_console.py
+"""
+
+import unittest
+
+class TestConsole(unittest.TestCase):
+    def try_help(self):
