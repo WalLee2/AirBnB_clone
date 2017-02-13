a3c171be8daa09ef17140a5274e492aa497c2357
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-12 23:52:30 -0800

    Initial Commit for testing suite

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Unit tests for <file>
+Run with: python -m unittest test_module
+"""
+import unittest
+
+class GenericTestingMethod(unittest.TestCase):
+    def something():
+        self.do_something()
+
+if __name__ == "__main__":
+    unittest.main()
