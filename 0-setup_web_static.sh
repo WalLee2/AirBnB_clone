d15540459c028ace9dd194ce223e7c446754df08
Walton Lee <mr.waltonlee@me.com>
2017-03-15 20:51:14 +0000

    fixing errors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Bash Script that sets up webservers for deployment.
 sudo apt-get install -y nginx
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-"$touch /data/web_static/releases/test/index.html"
-"$ln -fs /data/web_static/current" "/data/web_static/releases/test/"
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "29i\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
+sudo sed -i "29i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
