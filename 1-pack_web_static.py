e8d11173875283feeba44e02dc696f3432b57fd9
Walton Lee <mr.waltonlee@me.com>
2017-03-15 23:21:10 +0000

    fixing error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+Importing the fabric api
+"""
 from fabric.api import *
 
 
 def do_pack():
+    """
+    Making a versions directory and making a tarball
+    """
     try:
         local("sudo mkdir -p versions/")
         local("sudo tar -czvf \"./versions/web_static `date +%Y-%m-%d-%h-%M-%S`\
