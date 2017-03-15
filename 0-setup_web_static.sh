120fde53a443639c11551e536d9bb6338cb10e14
Walton Lee <mr.waltonlee@me.com>
2017-03-15 21:18:23 +0000

    modifying files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 # Bash Script that sets up webservers for deployment.
+sudo apt -get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo -e "<!DOCTYPE html>\n<html>\n\t<head>\n\t<\head>\n\t<body>\n\t Holberton School \n\t<\body>\n</html>" | sudo tee --append /data/web_static/releases/test/index.html
-sudo ln -fs "/data/web_static/current" "/data/web_static/releases/test/"
+echo -e '<!DOCTYPE html>\n<html>\n\t<head>\n\t<\head>\n\t<body>\n\t Holberton School \n\t<\body>\n</html>' | sudo tee --append /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "29i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
