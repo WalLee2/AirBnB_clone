02036e5542683c05a546954cf8d71c89d49dcbe0
Walton Lee <mr.waltonlee@me.com>
2017-03-15 22:56:55 +0000

    importing the Fabric library

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+from fabric.api import *
 
 
 
 def do_pack():
     try:
         local("sudo mkdir -p versions/")
-        local("sudo tar -czvf \"./version/web_static_ `date +%Y-%m-%d-%h-%M-%S`\
+        local("sudo tar -czvf \"./versions/web_static `date +%Y-%m-%d-%h-%M-%S`\
         .tgz\" web_static")
     except:
         return None
