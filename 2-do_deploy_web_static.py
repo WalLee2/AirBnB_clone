755a14e6f9fb677c60c5e3faa0668fe87744cc25
Walton Lee <mr.waltonlee@me.com>
2017-03-16 07:08:33 +0000

    fixing errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,8 +19,8 @@ def do_deploy(archive_path):
         target_name = archive_path.strip("versions/")
 
         put(archive_path, "/tmp/")
-        mystr_0 = "sudo mkdir -p /data/web_static/releases/" + target_name +
-        "/"
+        mystr_0 = "sudo mkdir -p /data/web_static/releases/" + target_name + \
+                  "/"
         run(mystr_0)
         mystr_1 = "sudo tar -xzf /tmp/" + target_name + \
                   " -C /data/web_static/releases/" + target_name + "/"
@@ -28,8 +28,8 @@ def do_deploy(archive_path):
         mystr_2 = "sudo rm /tmp/" + target_name
         run(mystr_2)
         mystr_3 = "sudo mv /data/web_static/releases/" + target_name + \
-                  "/web_static/* /data/web_static/releases/" + target_name +
-        "/"
+                  "/web_static/* /data/web_static/releases/" + target_name + \
+                  "/"
         run(mystr_3)
         mystr_4 = "sudo rm -rf /data/web_static/releases/" + target_name + \
                   "/web_static"
@@ -37,7 +37,7 @@ def do_deploy(archive_path):
         mystr_5 = "sudo rm -rf /data/web_static/current"
         run(mystr_5)
         mystr_6 = "sudo ln -s /data/web_static/releases/" + target_name + \
-                  "/ /data/web_static/current"
+                  " /data/web_static/current"
         run(mystr_6)
         print("Executing task 'deploy'")
         return True
