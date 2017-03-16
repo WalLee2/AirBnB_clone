a0a06f24e7d7f433fbec67316a22ba125514f54c
Walton Lee <mr.waltonlee@me.com>
2017-03-16 07:05:56 +0000

    fixing max line error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,23 +19,25 @@ def do_deploy(archive_path):
         target_name = archive_path.strip("versions/")
 
         put(archive_path, "/tmp/")
-        mystr_0 = "sudo mkdir -p /data/web_static/releases/" + target_name + "/"
+        mystr_0 = "sudo mkdir -p /data/web_static/releases/" + target_name +
+        "/"
         run(mystr_0)
         mystr_1 = "sudo tar -xzf /tmp/" + target_name + \
-        " -C /data/web_static/releases/" + target_name + "/"
+                  " -C /data/web_static/releases/" + target_name + "/"
         run(mystr_1)
         mystr_2 = "sudo rm /tmp/" + target_name
         run(mystr_2)
         mystr_3 = "sudo mv /data/web_static/releases/" + target_name + \
-        "/web_static/* /data/web_static/releases/" + target_name + "/"
+                  "/web_static/* /data/web_static/releases/" + target_name +
+        "/"
         run(mystr_3)
         mystr_4 = "sudo rm -rf /data/web_static/releases/" + target_name + \
-        "/web_static"
+                  "/web_static"
         run(mystr_4)
         mystr_5 = "sudo rm -rf /data/web_static/current"
         run(mystr_5)
         mystr_6 = "sudo ln -s /data/web_static/releases/" + target_name + \
-        " /data/web_static/current"
+                  "/ /data/web_static/current"
         run(mystr_6)
         print("Executing task 'deploy'")
         return True
